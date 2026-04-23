d5afcad16df81e62ad25fc65b6463f23c6f663d4
Aluno <aluno@fatecitu.local>
2026-04-23 10:00:46 -0300

    feat: adicionar Form2 (Jogo da Velha)

---
+++ WinFormsAppJulia/Form2.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WinFormsAppJulia
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            lblStatus = new Label();
+            btnReset = new Button();
+            label = new Label();
+            SuspendLayout();
+            // 
+            // btn1
+            // 
+            btn1.BackColor = Color.MintCream;
+            btn1.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn1.Location = new Point(80, 89);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(75, 59);
+            btn1.TabIndex = 0;
+            btn1.TabStop = false;
+            btn1.UseVisualStyleBackColor = false;
+            btn1.Click += btn_Click;
+            // 
+            // btn2
+            // 
+            btn2.BackColor = Color.MintCream;
+            btn2.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn2.Location = new Point(161, 89);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(75, 59);
+            btn2.TabIndex = 1;
+            btn2.TabStop = false;
+            btn2.UseVisualStyleBackColor = false;
+            btn2.Click += btn_Click;
+            // 
+            // btn3
+            // 
+            btn3.BackColor = Color.MintCream;
+            btn3.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn3.Location = new Point(242, 89);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(75, 59);
+            btn3.TabIndex = 2;
+            btn3.TabStop = false;
+            btn3.UseVisualStyleBackColor = false;
+            btn3.Click += btn_Click;
+            // 
+            // btn4
+            // 
+            btn4.BackColor = Color.MintCream;
+            btn4.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn4.Location = new Point(80, 154);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(75, 59);
+            btn4.TabIndex = 3;
+            btn4.TabStop = false;
+            btn4.UseVisualStyleBackColor = false;
+            btn4.Click += btn_Click;
+            // 
+            // btn5
+            // 
+            btn5.BackColor = Color.MintCream;
+            btn5.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn5.Location = new Point(161, 154);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(75, 59);
+            btn5.TabIndex = 4;
+            btn5.TabStop = false;
+            btn5.UseVisualStyleBackColor = false;
+            btn5.Click += btn_Click;
+            // 
+            // btn6
+            // 
+            btn6.BackColor = Color.MintCream;
+            btn6.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn6.Location = new Point(242, 154);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(75, 59);
+            btn6.TabIndex = 5;
+            btn6.TabStop = false;
+            btn6.UseVisualStyleBackColor = false;
+            btn6.Click += btn_Click;
+            // 
+            // btn7
+            // 
+            btn7.BackColor = Color.MintCream;
+            btn7.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn7.Location = new Point(80, 219);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(75, 59);
+            btn7.TabIndex = 6;
+            btn7.TabStop = false;
+            btn7.UseVisualStyleBackColor = false;
+            btn7.Click += btn_Click;
+            // 
+            // btn8
+            // 
+            btn8.BackColor = Color.MintCream;
+            btn8.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn8.Location = new Point(161, 219);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(75, 59);
+            btn8.TabIndex = 7;
+            btn8.TabStop = false;
+            btn8.UseVisualStyleBackColor = false;
+            btn8.Click += btn_Click;
+            // 
+            // btn9
+            // 
+            btn9.BackColor = Color.MintCream;
+            btn9.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn9.Location = new Point(242, 219);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(75, 59);
+            btn9.TabIndex = 8;
+            btn9.TabStop = false;
+            btn9.UseVisualStyleBackColor = false;
+            btn9.Click += btn_Click;
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(125, 301);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(0, 15);
+            lblStatus.TabIndex = 10;
+            // 
+            // btnReset
+            // 
+            btnReset.BackColor = Color.MintCream;
+            btnReset.ForeColor = Color.FromArgb(64, 64, 64);
+            btnReset.Location = new Point(80, 355);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(237, 29);
+            btnReset.TabIndex = 11;
+            btnReset.Text = "Resetar";
+            btnReset.UseVisualStyleBackColor = false;
+            btnReset.Click += btnReset_Click;
+            // 
+            // label
+            // 
+            label.AutoSize = true;
+            label.ForeColor = Color.FromArgb(64, 64, 64);
+            label.Location = new Point(83, 301);
+            label.Name = "label";
+            label.Size = new Size(42, 15);
+            label.TabIndex = 12;
+            label.Text = "Status:";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.MintCream;
+            ClientSize = new Size(389, 450);
+            Controls.Add(label);
+            Controls.Add(btnReset);
+            Controls.Add(lblStatus);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn1;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Label lblStatus;
+        private Button btnReset;
+        private Label label;
+    }
+}
---
+++ WinFormsAppJulia/Form2.cs
@@ -0,0 +1,77 @@
+﻿namespace WinFormsAppJulia
+{
+    public partial class Form2 : Form
+    {
+        bool isXTurn;
+        int turnCount;
+        bool gameOver;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object? sender, EventArgs e)
+        {
+            if (gameOver) { return; }
+
+            Button btn = (Button)sender;
+
+            if (btn != null && string.IsNullOrEmpty(btn.Text))
+            {
+                btn.Text = isXTurn ? "X" : "O";
+                turnCount++;
+                isXTurn = !isXTurn;
+
+                if (CheckForWinner())
+                {
+                    lblStatus.Text = $"Jogador {(isXTurn ? "O" : "X")} ganhou!";
+                    gameOver = true;
+                }
+                else if (turnCount == 9)
+                {
+                    lblStatus.Text = "Deu velha!";
+                    gameOver = true;
+                }
+                else
+                {
+                    lblStatus.Text = $"É a vez do jogador {(isXTurn ? "X" : "O")}";
+                }
+            }
+        }
+
+        private bool CheckForWinner()
+        {
+            string[,] matriz = new string[3, 3]
+            {
+                { btn1.Text, btn2.Text, btn3.Text },
+                { btn4.Text, btn5.Text, btn6.Text },
+                { btn7.Text, btn8.Text, btn9.Text }
+            };
+            for (int i = 0; i < 3; i++)
+            {
+                if (!string.IsNullOrEmpty(matriz[i, 0]) && matriz[i, 0] == matriz[i, 1] && matriz[i, 1] == matriz[i, 2]) return true;
+                if (!string.IsNullOrEmpty(matriz[0, i]) && matriz[0, i] == matriz[1, i] && matriz[1, i] == matriz[2, i]) return true;
+            }
+            if (!string.IsNullOrEmpty(matriz[0, 0]) && matriz[0, 0] == matriz[1, 1] && matriz[1, 1] == matriz[2, 2]) return true;
+            if (!string.IsNullOrEmpty(matriz[0, 2]) && matriz[0, 2] == matriz[1, 1] && matriz[1, 1] == matriz[2, 0]) return true;
+            return false;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            foreach (var control in Controls)
+            {
+                if (control is Button btn && btn != btnReset)
+                {
+                    btn.Text = string.Empty;
+                }
+            }
+
+            isXTurn = false;
+            turnCount = 0;
+            gameOver = false;
+            lblStatus.Text = "É a vez do jogador O";
+        }
+    }
+}
